f80cf62571ded14f46e4612d0a5a723e7878b3f3
GoomezCode <danielgdpaula@gmail.com>
2025-03-06 21:33:49 -0300

    .

--- BackEnd/projetoJava/src/ClassJava.classes/ObjetosComputador.java
+++ BackEnd/projetoJava/src/ClassJava.classes/ObjetosComputador.java
@@ -1,4 +1,15 @@
 package ClassJava.Class;
 
-
+public class ObjetosComputador {
+    
+    private String PlacaMae;
+    private String Fonte;
+    private String Processador;
+    private String HD;
+    private String SSD;
+    private String PlacaVideo;
+    private String Memory;
+    private String OrganizacaoCabo;
+                      
+}
 
