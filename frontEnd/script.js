70f4abfd4a94b247fa84a28c9933d3b78b6f9c3c
GoomezCode <danielgdpaula@gmail.com>
2025-03-08 07:45:07 -0300

    .

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,1 +1,12 @@
-/*não sei mexer ainda*/
+document.getElementById("irRegistro").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("minhaDiv").classList.toggle("MoverDivLogin");
+    
+})
+
+document.getElementById("irLogin").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("minhaDiv").classList.toggle("MoverDivLogin");
+    })
+
+    
